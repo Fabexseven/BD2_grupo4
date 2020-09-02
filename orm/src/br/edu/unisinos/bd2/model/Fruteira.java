5d52c7cbbd5e6f40ea380af3f73f0a7b8f547282
fabiokrein <fabio.krein@sap.com>
2020-09-02 16:22:33 -0300

    Initial Version

---
+++ orm/src/br/edu/unisinos/bd2/model/Fruteira.java
@@ -0,0 +1,23 @@
+package br.edu.unisinos.bd2.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Fruteira implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "s_Fruteira")
+	@SequenceGenerator(name = "s_Fruteira", sequenceName = "s_Fruteira", allocationSize = 1)
+	private Long id;
+	
+	@Column(length = 50, nullable = false)
+	private String nome;
+}
